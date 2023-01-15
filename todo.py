4ab2e527859a5d3a9c47415d5273a91a1e97996f
DHARMESH PRASAD <98016812+dharmeshim@users.noreply.github.com>
2023-01-15 13:24:41 +0530

    Add files via upload

--- todo.py
+++ todo.py
@@ -1,102 +1,82 @@
-import tkinter as tk
-from tkinter import ttk
-import sqlite3
+import json
 
-conn = sqlite3.connect('todolist.db')
-c = conn.cursor()
+class ToDoList:
+    def __init__(self, name):
+        self.name = name
+        self.tasks = []
+        self.completed_tasks = []
+        self.num_tasks = 0
+        self.read_tasks_from_file()
 
-def create_table():
-    c.execute("CREATE TABLE IF NOT EXISTS todo_list (task text, due_date text, status text)")
-    conn.commit()
+    def add_task(self, task):
+        self.tasks.append(task)
+        self.num_tasks += 1
+        self.write_tasks_to_file()
 
-create_table()
+    def mark_task_complete(self, task):
+        self.tasks.remove(task)
+        self.completed_tasks.append(task)
+        self.num_tasks -= 1
+        self.write_tasks_to_file()
 
-def add_item():
-    task = task_entry.get()
-    due_date = date_entry.get()
-    status = status_entry.get()
-    c.execute("INSERT INTO todo_list (task, due_date, status) VALUES (?, ?, ?)",(task, due_date, status))
-    conn.commit()
-    task_entry.delete(0,tk.END)
-    date_entry.delete(0,tk.END)
-    status_entry.delete(0,tk.END)
-    view_list()
+    def display_tasks(self):
+        print("Tasks in list '{}':".format(self.name))
+        for task in self.tasks:
+            print("- {}".format(task))
 
-def view_list():
-    c.execute("SELECT * FROM todo_list WHERE status='incomplete'")
-    rows = c.fetchall()
-    for row in rows:
-        print(row)
-    c.execute("SELECT * FROM todo_list WHERE status='complete'")
-    rows = c.fetchall()
-    for row in rows:
-        print(row)
+    def display_completed_tasks(self):
+        print("Completed Tasks in list '{}':".format(self.name))
+        for task in self.completed_tasks:
+            print("- {}".format(task))
 
-def update_status():
-    task = task_entry.get()
-    status = status_entry.get()
-    c.execute("UPDATE todo_list SET status = ? WHERE task = ?", (status, task))
-    conn.commit()
-    task_entry.delete(0,tk.END)
-    date_entry.delete(0,tk.END)
-    status_entry.delete(0,tk.END)
-    view_list()
+    def write_tasks_to_file(self):
+        tasks = {'tasks': self.tasks, 'completed_tasks': self.completed_tasks}
+        with open('tasks.json', 'w') as file:
+            json.dump(tasks, file)
+    def read_tasks_from_file(self):
+        try:
+            with open('tasks.json', 'r') as file:
+                tasks = json.load(file)
+                self.tasks = tasks['tasks']
+                self.completed_tasks = tasks['completed_tasks']
+                self.num_tasks = len(self.tasks)
+        except FileNotFoundError:
+            # If the file doesn't exist, it means that no tasks have been added yet
+            pass
 
-def delete_item():
-    task = task_entry.get()
-    c.execute("DELETE from todo_list WHERE task = ?", (task,))
-    conn.commit()
-    task_entry.delete(0,tk.END)
-    date_entry.delete(0,tk.END)
-    status_entry.delete(0,tk.END)
-    view_list()
+if __name__ == "__main__":
+    my_list = ToDoList("My To-Do List")
 
-def search_list():
-    query = search_entry.get()
-    c.execute("SELECT * FROM todo_list WHERE task like ? OR due_date like ? OR status like ?", ('%'+query+'%', '%'+query+'%','%'+query+'%'))
-    rows = c.fetchall()
-    for row in rows:
-        print(row)
-    search_entry.delete(0,tk.END)
+    while True:
+        print("1. Add task")
+        print("2. Mark task as complete")
+        print("3. Display tasks")
+        print("4. Display completed tasks")
+        print("5. Exit")
 
-root = tk.Tk()
-root.title("To-Do List")
+        choice = int(input("Enter your choice: "))
 
-task_label = ttk.Label(root, text="Task:")
-task_label.grid(row=0, column=0)
+        if choice == 1:
+            task = input("Enter task: ")
+            my_list.add_task(task)
+        elif choice == 2:
+            task = input("Enter task to mark as complete: ")
+            my_list.mark_task_complete(task)
+        elif choice == 3:
+            my_list.display_tasks()
+        elif choice == 4:
+            my_list.display_completed_tasks()
+        elif choice == 5:
+            break
+        else:
+            print("Invalid choice.")
 
-task_entry = ttk.Entry(root)
-task_entry.grid(row=0, column=1)
 
-date_label = ttk.Label(root, text="Date:")
-date_label.grid(row=1, column=0)
 
-date_entry = ttk.Entry(root)
-date_entry.grid(row=1, column=1)
 
-status_label = ttk.Label(root, text="Status:")
-status_label.grid(row=2, column=0)
 
-status_entry = ttk.Entry(root)
-status_entry.grid(row=2, column=1)
 
-add_button = ttk.Button(root, text="Add", command=add_item)
-add_button.grid(row=3, column=0, pady=10)
 
-update_button = ttk.Button(root, text="Update", command=update_status)
-update_button.grid(row=3, column=1)
 
-delete_button = ttk.Button(root, text="Delete", command=delete_item)
-delete_button.grid(row=3, column=2)
 
-search_label = ttk.Label(root, text="Search:")
-search_label.grid(row=4, column=0)
-
-search_entry = ttk.Entry(root)
-search_entry.grid(row=4, column=1)
-
-search_button = ttk.Button(root, text="Search", command=search_list)
-search_button.grid(row=4, column=2)
-
-root.mainloop()
 
